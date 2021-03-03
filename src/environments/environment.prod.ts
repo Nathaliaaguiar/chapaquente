7185c6d8f4461ca2b332cbca2499ab6217eab373
Nathaliaaguiar <59691090+Nathaliaaguiar@users.noreply.github.com>
2021-03-03 11:44:06 -0300

    aula_03

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCacvxgz-W3thPmTkqtDVWv9JXWl67NJjo",
+    authDomain: "projeto-nathalia-aguiar.firebaseapp.com",
+    projectId: "projeto-nathalia-aguiar",
+    storageBucket: "projeto-nathalia-aguiar.appspot.com",
+    messagingSenderId: "809522703078",
+    appId: "1:809522703078:web:a2be3e720e453543bbe927"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCacvxgz-W3thPmTkqtDVWv9JXWl67NJjo",
+    authDomain: "projeto-nathalia-aguiar.firebaseapp.com",
+    projectId: "projeto-nathalia-aguiar",
+    storageBucket: "projeto-nathalia-aguiar.appspot.com",
+    messagingSenderId: "809522703078",
+    appId: "1:809522703078:web:a2be3e720e453543bbe927"
+  }
 };
 
 /*
