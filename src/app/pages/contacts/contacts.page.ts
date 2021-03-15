b2730ac60e945644751f4b2c2a649cf23f24049d
Nathaliaaguiar <59691090+Nathaliaaguiar@users.noreply.github.com>
2021-03-15 11:48:33 -0300

    app02

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,6 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
 
-// 1) Importa dependências
+// 1) Importa dependÃªncias
 import {
   FormBuilder,
   FormGroup,
@@ -10,7 +11,10 @@ import {
 import { AngularFirestore } from '@angular/fire/firestore';
 import { DatePipe } from '@angular/common';
 
-// 6) Não permite somente espaços nos campos
+// Alert Controller
+import { AlertController } from '@ionic/angular';
+
+// 6) NÃ£o permite somente espaÃ§os nos campos
 export function removeSpaces(control: AbstractControl) {
   if (control && control.value && !control.value.replace(/\s/g, '').length) {
     control.setValue('');
@@ -25,48 +29,116 @@ export function removeSpaces(control: AbstractControl) {
 })
 export class ContactsPage implements OnInit {
   // 3) Atributos
-  public contactForm: FormGroup; // Contém o formulário de contatos
+  public contactForm: FormGroup; // ContÃ©m o formulÃ¡rio de contatos
   public pipe = new DatePipe('en_US'); // Formatar as datas
 
   constructor(
-    // 2) Injeta dependências
+    // 2) Injeta dependÃªncias
     public form: FormBuilder,
-    public firestore: AngularFirestore
-  ) {}
+    public firestore: AngularFirestore,
+
+    // Alert Controller
+    public alert: AlertController
+  ) { }
 
   ngOnInit() {
-    // 4) Cria o formulário de contatos
+    // 4) Cria o formulÃ¡rio de contatos
     this.contactFormCreate();
   }
 
-  // 5) Cria o formulário de contatos
+  // 5) Cria o formulÃ¡rio de contatos
   contactFormCreate() {
+
+    // 'contactForm' contÃ©m o formulÃ¡rio
+    // Um formulÃ¡rio Ã© um 'agrupamento' (group) de campos...
     this.contactForm = this.form.group({
-      date: [''], // Data de envio
+
+      // Data de envio estÃ¡ vazia
+      date: [''],
+
+      // Campo 'Nome' (name)
       name: [
-        // Nome
+        '', // Valor inicial
+        Validators.compose([ // ValidaÃ§Ã£o do campo
+          Validators.required, // ObrigatÃ³rio
+          Validators.minLength(3), // Pelo menos 3 caracteres
+          removeSpaces // NÃ£o permite somente espaÃ§os
+        ]),
+      ],
+
+      // Campo 'E-mail' (email)
+      email: [
         '',
         Validators.compose([
-          Validators.required, // Obrigatório
-          Validators.minLength(3), // Nomes curtos demais
+          Validators.required,
+          Validators.email, // Valida somente se for um e-mail vÃ¡lido
           removeSpaces
         ]),
       ],
-      email: [
-        // E-mail
-        '',
-        Validators.compose([Validators.required, Validators.email, removeSpaces]),
-      ],
+
+      // Campo 'Assunto' (subject)
       subject: [
-        // Assunto
         '',
-        Validators.compose([Validators.required, Validators.minLength(5), removeSpaces]),
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          removeSpaces
+        ]),
       ],
       message: [
         // Mensagem
         '',
-        Validators.compose([Validators.required, Validators.minLength(5), removeSpaces]),
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          removeSpaces
+        ]),
       ],
     });
+  }
+
+  // 7) Processa o envio do formulÃ¡rio]
+  contactSend() {
+
+    // Cria e formata a data
+    this.contactForm.controls.date.setValue(
+      this.pipe.transform(Date.now(), 'yyyy-MM-dd HH:mm:ss').trim()
+    );
+
+    // Salva em um novo documento do Firebase Firestore
+    this.firestore.collection('contacts').add(this.contactForm.value)
+      .then(
+        () => {
+
+          // Feedback
+          this.presentAlert();
+        }
+      )
+      .catch(
+
+        // Exibe erro se nÃ£o salvar
+        (error) => {
+          alert('Erro ao salvar contato.' + error);
+        }
+      );
+  }
+
+  // Feedback
+  // Exibe feedback
+  async presentAlert() {
+    const alert = await this.alert.create({
+      header: 'Oba!',
+      message: 'Contato enviado com sucesso!',
+      buttons: [{
+        text: 'Ok',
+        handler: () => {
+          
+          // Reset do formulÃ¡rio
+          this.contactForm.reset();
+        }
+      }]
+    });
+
+    await alert.present();
   }
 }
